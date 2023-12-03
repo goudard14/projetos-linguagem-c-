476fb1eb64ea65b4ffcf2975911901ae5e9e5191
Gabriel Goudard <143735800+goudard14@users.noreply.github.com>
2023-12-03 18:12:43 -0300

    Create gato..c

---
+++ gato..c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void miau (int n);
+
+int main(void)
+{
+    miau(3);
+}
+
+void miau(int n)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        printf("miau.\n");
+    }
+}
